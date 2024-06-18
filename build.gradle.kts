6478625b0251b438e384d9ee6c09c7cf21efdde2
Dragon-Seeker <timekeeperguild@gmail.com>
2024-06-18 17:19:57 -0500

    Attempt to fix publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import groovy.json.JsonSlurper
 import net.fabricmc.loom.build.nesting.IncludedJarFactory
 import net.fabricmc.loom.build.nesting.JarNester
+import net.fabricmc.loom.task.RemapJarTask
 import net.fabricmc.loom.util.Constants
-import net.fabricmc.loom.util.GroovyXmlUtil
 import org.apache.commons.codec.digest.DigestUtils
 import org.eclipse.jgit.api.Git
+import java.net.URI
 import java.util.*
 
 plugins {
@@ -26,9 +28,9 @@ val DEV_ONLY_MODULES: List<String> = listOf(
     "fabric-gametest-api-v1"
 )
 
-ext["getSubprojectVersion"] = object : groovy.lang.Closure<Unit>(this) {
-    fun doCall(project: Project) {
-        getSubprojectVersion(project)
+ext["getSubprojectVersion"] = object : groovy.lang.Closure<String>(this) {
+    fun doCall(project: Project) : String {
+        return getSubprojectVersion(project)
     }
 }
 ext["moduleDependencies"] = object : groovy.lang.Closure<Unit>(this) {
@@ -126,6 +128,8 @@ tasks {
 
 // Subprojects
 
+subprojects { group = "org.sinytra.fabric-api"  }
+
 allprojects {
     val modDependencies: Configuration by configurations.creating
 
@@ -140,7 +144,7 @@ allprojects {
     apply(plugin = "ffapi.package-info")
 
     if (!META_PROJECTS.contains(name) && project != rootProject) {
-//        apply(plugin = "ffapi.neo-compat") TODO
+        apply(plugin = "ffapi.neo-compat")
     }
 
     allprojects.forEach { p ->
@@ -161,11 +165,37 @@ allprojects {
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     }
 
+    tasks.withType<ProcessResources> {
+        inputs.property("version", project.version)
+
+        filesMatching("fabric.mod.json") {
+            expand(Pair("version", project.version))
+        }
+    }
+
     publishing {
         publications {
             register<MavenPublication>("mavenJava") {
+                val remapReferenceApi = tasks.named<RemapJarTask>("remapReferenceApi").get();
+
                 pom {
-//                    addPomMetadataInformation(project, pom) TODO
+                    addPomMetadataInformation(project, pom)
+                }
+
+                artifact(remapReferenceApi) {
+                    builtBy(remapReferenceApi)
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                val ENV = System.getenv();
+
+                url = URI.create(ENV["MAVEN_URL"] ?: "")
+                credentials {
+                    username = ENV["MAVEN_USER"]
+                    password = ENV["MAVEN_PASSWORD"]
                 }
             }
         }
@@ -185,24 +215,34 @@ dependencies {
 	}
 }
 
-publishing {
-    publications {
-        named<MavenPublication>("mavenJava") {
-            from(components["java"])
-
-            pom.withXml {
-                val depsNode = GroovyXmlUtil.getOrCreateNode(asNode(), "dependencies")
-                rootProject.configurations.include.get().dependencies.forEach {
-                    val depNode = depsNode.appendNode("dependency")
-                    depNode.appendNode("groupId", it.group)
-                    depNode.appendNode("artifactId", it.name)
-                    depNode.appendNode("version", it.version)
-                    depNode.appendNode("scope", "compile")
-                }
-            }
-        }
-    }
-}
+//publishing {
+//    publications {
+//        named<MavenPublication>("mavenJava") {
+//            pom.withXml {
+//                val depsNode = asNode().appendNode("dependencies")
+//                subprojects.forEach {
+//                    // The maven BOM containing all of the deprecated modules is added manually below.
+//                    if (it.path.startsWith(":deprecated") || META_PROJECTS.contains(it.name)) {
+//                        return@forEach
+//                    }
+//
+//                    val depNode = depsNode.appendNode("dependency")
+//                    depNode.appendNode("groupId", it.group)
+//                    depNode.appendNode("artifactId", it.name)
+//                    depNode.appendNode("version", it.version)
+//                    depNode.appendNode("scope", "compile")
+//                }
+//
+//                // Depend on the deprecated BOM to allow opting out of deprecated modules.
+//                val depNode = depsNode.appendNode("dependency")
+//                depNode.appendNode("groupId", group)
+//                depNode.appendNode("artifactId", "fabric-api-deprecated")
+//                depNode.appendNode("version", version)
+//                depNode.appendNode("scope", "compile")
+//            }
+//        }
+//    }
+//}
 
 val git: Git? = runCatching { Git.open(rootDir) }.getOrNull()
 
@@ -258,5 +298,39 @@ fun testDependencies(project: Project, depNames: List<String>) {
         deps.forEach {
             "testmodImplementation"(it)
         }
+    }
+}
+
+fun addPomMetadataInformation(project: Project, pom: MavenPom) {
+    var modJsonFile = project.file("src/main/resources/fabric.mod.json")
+
+    if (!modJsonFile.exists()) {
+        modJsonFile = project.file("src/client/resources/fabric.mod.json")
+    }
+
+    val modJson = JsonSlurper().parse(modJsonFile) as Map<*, *>
+    pom.name = modJson["name"] as String
+    pom.url = "https://github.com/FabricMC/fabric/tree/HEAD/${project.rootDir.relativeTo(project.projectDir)}"
+    pom.description = modJson["description"] as String
+    pom.licenses {
+        license {
+            name = "Apache-2.0"
+            url = "https://github.com/FabricMC/fabric/blob/HEAD/LICENSE"
+        }
+    }
+    pom.developers {
+        developer {
+            name = "FabricMC"
+            url = "https://fabricmc.net/"
+        }
+    }
+    pom.scm {
+        connection = "scm:git:https://github.com/FabricMC/fabric.git"
+        url = "https://github.com/FabricMC/fabric"
+        developerConnection = "scm:git:git@github.com:FabricMC/fabric.git"
+    }
+    pom.issueManagement {
+        system = "GitHub"
+        url = "https://github.com/FabricMC/fabric/issues"
     }
 }
